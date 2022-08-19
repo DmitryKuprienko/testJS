354b0e4b0f9a3331c56733556f9ad6d2dc259469
DmitryKuprienko <101987031+DmitryKuprienko@users.noreply.github.com>
2022-08-19 10:21:14 +0300

    тест ЖС

---
+++ js/test.js
@@ -0,0 +1,54 @@
+// Змінні, оголошені як const, обов'язково повинні бути ініціалізовані
+// значенням під час оголошення, інакше виникне помилка.
+const yearOfBirth = 2006;
+console.log(yearOfBirth); // 2006
+
+// Змінним, оголошеним через let, не обов'язково одразу присвоювати значення.
+let age;
+
+// Якщо змінній, оголошеній як let, не було присвоєно значення,
+// вона ініціалізується спеціальним значенням undefined (не визначено).
+console.log(age); // undefined
+
+// console.log() - це метод для виведення даних у консоль браузера,
+// пізніше познайомимось з ним детальніше.
+
+// Якщо змінна оголошена як let, її значення можна перезаписати.
+age = 1600;
+console.log(age); // 14
+
+
+
+
+let username;
+console.log(typeof username); // "undefined"
+
+let inputValue = null;
+console.log(typeof inputValue); // "object"
+
+const quantity = 17;
+console.log(typeof quantity); // "number"
+
+const message = "JavaScript is awesome!";
+console.log(typeof message); // "string"
+
+const isSidebarOpen = false;
+console.log(typeof isSidebarOpen); // "boolean"
+
+
+// console.log(message, quantity,); // JavaScript is awesome!
+// alert(message);
+
+// Просимо клієнта підтвердити бронювання готелю
+// і зберігаємо результат роботи confirm у змінну
+const isComing = confirm("Please confirm hotel reservation");
+console.log(isComing);
+
+// Запитуємо назву готеля, в якому хотів би зупинитися клієнт
+// і зберігаємо результат виклику prompt у змінну.
+const hotelName = prompt("Please enter desired hotel name");
+console.log(hotelName);
+
+const value = prompt("Please enter a number!");
+console.log(typeof value); // "string"
+console.log(value); // "5"
