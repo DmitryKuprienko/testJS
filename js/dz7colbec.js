4e0b1b702149ecb24f601dd6963129e38cdb2cf2
DmitryKuprienko <101987031+DmitryKuprienko@users.noreply.github.com>
2022-09-14 01:01:25 +0300

    застряв на 13

--- js/dz7colbec.js
+++ js/dz7colbec.js
@@ -123,34 +123,154 @@
 // processCall("Mango");
 // =============================================
 
-const pizzaPalace = {
-  pizzas: ["Ultracheese", "Smoked", "Four meats"],
-  order(pizzaName, onSuccess, onError) {
-    for (const pizza of this.pizzas) {
-      if (pizza === pizzaName) {
-        console.log(onSuccess(pizzaName));
-      }
+// const pizzaPalace = {
+//   pizzas: ["Ultracheese", "Smoked", "Four meats"],
+//   order(pizzaName, onSuccess, onError) {
+//     for (const pizza of this.pizzas) {
+//       if (pizza === pizzaName) {
+//         console.log(onSuccess(pizzaName));
+//       }
+//     }
+//     console.log(
+//       onError(`There is no pizza with a name ${pizzaName} in the assortment.`)
+//     );
+//   },
+// };
+
+// // Change code above this line
+
+// // Callback for onSuccess
+// function makePizza(pizzaName) {
+//   return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+// }
+
+// // Callback for onError
+// function onOrderError(error) {
+//   return `Error! ${error}`;
+// }
+
+// // Method calls with callbacks
+// pizzaPalace.order("Smoked", makePizza, onOrderError);
+// pizzaPalace.order("Four meats", makePizza, onOrderError);
+// pizzaPalace.order("Big Mike", makePizza, onOrderError);
+// pizzaPalace.order("Vienna", makePizza, onOrderError);
+// // =========================================
+
+// Функція getCommonElements(firstArray, secondArray) приймає два масиви довільної довжини в параметри firstArray і secondArray,
+//   і повертає новий масив їхніх спільних елементів, тобто тих, які присутні в обох масивах.
+
+// Виконай рефакторинг функції таким чином, щоб замість циклу for, вона використовувала метод forEach.
+
+// Оголошена функція getCommonElements(firstArray, secondArray)
+// Для перебирання параметра (масиву) використаний метод forEach
+// Виклик getCommonElements([1, 2, 3], [2, 4]) повертає [2]
+// Виклик getCommonElements([1, 2, 3], [2, 1, 17, 19]) повертає [1, 2]
+// Виклик getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]) повертає [12, 27, 3]
+// Виклик getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]) повертає [10, 30, 40]
+// Виклик getCommonElements([1, 2, 3], [10, 20, 30]) повертає []
+// Виклик функції з випадковими, але валідними аргументами, повертає правильне значення
+
+// function filterArray(numbers, value) {
+//   const filteredNumbers = [];
+//   // Change code below this line
+//   numbers.forEach((element) => {
+//     if (element > value) {
+//       filteredNumbers.push(element);
+//     }
+//   });
+//   // for (let i = 0; i < numbers.length; i += 1) {
+//   //   if (numbers[i] > value) {
+//   //     filteredNumbers.push(numbers[i]);
+//   //   }
+//   // }
+
+//   // Change code above this line
+//   return filteredNumbers;
+// }
+
+// console.log(filterArray([1, 2, 3, 4, 5], 3));
+// console.log(filterArray([1, 2, 3, 4, 5], 4));
+// console.log(filterArray([1, 2, 3, 4, 5], 5));
+// console.log(filterArray([12, 24, 8, 41, 76], 38));
+// console.log(filterArray([12, 24, 8, 41, 76]));
+
+// function getCommonElements(firstArray, secondArray) {
+//   const commonElements = [];
+//   // Change code below this line
+//   firstArray.forEach((element) => {
+//     if (secondArray.includes(element)) {
+//       commonElements.push(element);
+//     }
+//   });
+//   // for (let i = 0; i < firstArray.length; i += 1) {
+//   //   if (secondArray.includes(firstArray[i])) {
+//   //     commonElements.push(firstArray[i]);
+//   //   }
+//   // }
+
+//   return commonElements;
+//   // Change code above this line
+// }
+// console.log(getCommonElements([1, 2, 3], [2, 4])); // повертає [2]
+// console.log(getCommonElements([1, 2, 3], [2, 1, 17, 19])); // повертає [1, 2]
+// console.log(getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27])); // повертає [12, 27, 3]
+// console.log(getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40])); // повертає [10, 30, 40]
+// console.log(getCommonElements([1, 2, 3], [10, 20, 30])); // повертає []
+// ======================================================================
+// Change code below this line
+// const calculateTotalPrice = (quantity, pricePerItem) => quantity * pricePerItem;
+// // function calculateTotalPrice(quantity, pricePerItem) {
+// //   // Change code above this line
+// //   return quantity * pricePerItem;
+// // }
+
+// console.log(calculateTotalPrice(5, 100)); //повертає 500
+// console.log(calculateTotalPrice(8, 60)); //повертає 480
+// console.log(calculateTotalPrice(3, 400)); //повертає 1200
+// =======================================================================
+
+// Change code below this line
+// const filterArray = (numbers, value) => {
+//   const filteredNumbers = [];
+
+//   numbers.forEach((number) => {
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   });
+//   // Change code above this line
+//   return filteredNumbers;
+// };
+// console.log(filterArray([1, 2, 3, 4, 5], 3)); // повертає [4, 5]
+// console.log(filterArray([1, 2, 3, 4, 5], 4)); //повертає [5]
+// console.log(filterArray([1, 2, 3, 4, 5], 5)); //повертає []
+// console.log(filterArray([12, 24, 8, 41, 76], 38)); //повертає [41, 76]
+// console.log(filterArray([12, 24, 8, 41, 76], 20)); // повертає [24, 41, 76]
+// ==============================================================================
+const changeEven = (numbers, value) => {
+  // Change code below this line
+  const newArrey = [];
+
+  numbers.forEach((element) => {
+    if (element % 2 === 0) {
+      console.log(element);
+      newArrey.push(element + value);
+    } else {
+      newArrey.push(element);
     }
-    console.log(
-      onError(`There is no pizza with a name ${pizzaName} in the assortment.`)
-    );
-  },
+  });
+
+  // for (let i = 0; i < numbers.length; i += 1) {
+  //   if (numbers[i] % 2 === 0) {
+  //     numbers[i] = numbers[i] + value;
+  //   }
+  // }
+
+  return newArrey;
+  // Change code above this line
 };
 
-// Change code above this line
-
-// Callback for onSuccess
-function makePizza(pizzaName) {
-  return `Your order is accepted. Cooking pizza ${pizzaName}.`;
-}
-
-// Callback for onError
-function onOrderError(error) {
-  return `Error! ${error}`;
-}
-
-// Method calls with callbacks
-pizzaPalace.order("Smoked", makePizza, onOrderError);
-pizzaPalace.order("Four meats", makePizza, onOrderError);
-pizzaPalace.order("Big Mike", makePizza, onOrderError);
-pizzaPalace.order("Vienna", makePizza, onOrderError);
+console.log(changeEven([1, 2, 3, 4, 5], 10)); //повертає новий масив [1, 12, 3, 14, 5]
+console.log(changeEven([2, 8, 3, 7, 4, 6], 10)); //повертає новий масив [12, 18, 3, 7, 14, 16]
+console.log(changeEven([17, 24, 68, 31, 42], 100)); //повертає новий масив [17, 124, 168, 31, 142]
+console.log(changeEven([44, 13, 81, 92, 36, 54], 100)); //
