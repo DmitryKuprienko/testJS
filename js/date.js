9f16b475519b4ca4b31988af2617a3f3312f19e0
DmitryKuprienko <101987031+DmitryKuprienko@users.noreply.github.com>
2022-10-30 14:08:28 +0200

    5555

--- js/date.js
+++ js/date.js
@@ -1,52 +1,82 @@
 const date = new Date;
 
-console.log("date:", date)
-// Повертає день місяця від 1 до 31
-console.log("getDate(): ", date.getDate());
+// console.log("date:", date)
+// // Повертає день місяця від 1 до 31
+// console.log("getDate(): ", date.getDate());
 
-// Повертає день тижня від 0 до 6
-console.log("getDay(): ", date.getDay());
+// // Повертає день тижня від 0 до 6
+// console.log("getDay(): ", date.getDay());
 
-// Повертає місяць від 0 до 11
-console.log("getMonth(): ", date.getMonth());
+// // Повертає місяць від 0 до 11
+// console.log("getMonth(): ", date.getMonth());
 
-// Повертає рік з 4 цифр
-console.log("getFullYear(): ", date.getFullYear());
+// // Повертає рік з 4 цифр
+// console.log("getFullYear(): ", date.getFullYear());
 
-// Повертає години
-console.log("getHours(): ", date.getHours());
+// // Повертає години
+// console.log("getHours(): ", date.getHours());
 
-// Повертає хвилини
-console.log("getMinutes(): ", date.getMinutes());
+// // Повертає хвилини
+// console.log("getMinutes(): ", date.getMinutes());
 
-// Повертає секунди
-console.log("getSeconds(): ", date.getSeconds());
+// // Повертає секунди
+// console.log("getSeconds(): ", date.getSeconds());
 
-// Повертає мілісекунди
-console.log("getMilliseconds(): ", date.getMilliseconds());
+// // Повертає мілісекунди
+// console.log("getMilliseconds(): ", date.getMilliseconds());
 
 
 
-// Повертає день місяця від 1 до 31
-console.log("getUTCDate(): ", date.getUTCDate());
+// // Повертає день місяця від 1 до 31
+// console.log("getUTCDate(): ", date.getUTCDate());
 
-// Повертає день тижня від 0 до 6
-console.log("getUTCDay(): ", date.getUTCDay());
+// // Повертає день тижня від 0 до 6
+// console.log("getUTCDay(): ", date.getUTCDay());
 
-// Повертає місяць від 0 до 11
-console.log("getUTCMonth(): ", date.getUTCMonth());
+// // Повертає місяць від 0 до 11
+// console.log("getUTCMonth(): ", date.getUTCMonth());
 
-// Повертає рік з 4 цифр
-console.log("getUTCFullYear(): ", date.getUTCFullYear());
+// // Повертає рік з 4 цифр
+// console.log("getUTCFullYear(): ", date.getUTCFullYear());
 
-// Повертає години
-console.log("getUTCHours(): ", date.getUTCHours());
+// // Повертає години
+// console.log("getUTCHours(): ", date.getUTCHours());
 
-// Повертає хвилини
-console.log("getUTCMinutes(): ", date.getUTCMinutes());
+// // Повертає хвилини
+// console.log("getUTCMinutes(): ", date.getUTCMinutes());
 
-// Повертає секунди
-console.log("getUTCSeconds(): ", date.getUTCSeconds());
+// // Повертає секунди
+// console.log("getUTCSeconds(): ", date.getUTCSeconds());
 
-// Повертає мілісекунди
-console.log("getUTCMilliseconds(): ", date.getUTCMilliseconds());
+// // Повертає мілісекунди
+// console.log("getUTCMilliseconds(): ", date.getUTCMilliseconds());
+// ===========================================================================
+const isSuccess = true;
+
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (isSuccess) {
+      resolve("Success! Value passed to resolve function");
+    } else {
+      reject("Error! Error passed to reject function");
+    }
+  }, 2000);
+});
+console.log("Before promise.then()");
+
+// Registering promise callbacks
+promise.then(
+  // onResolve will run third or not at all
+  value => {
+    console.log("onResolve call inside promise.then()");
+    console.log(value); // "Success! Value passed to resolve function"
+  },
+  // onReject will run third or not at all
+  error => {
+    console.log("onReject call inside promise.then()");
+    console.log(error); // "Error! Error passed to reject function"
+  }
+);
+
+// Will run second
+console.log("After promise.then()");
