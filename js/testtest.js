c5f0675dd03589b0f432b8fd7e883b2513009967
DmitryKuprienko <101987031+DmitryKuprienko@users.noreply.github.com>
2022-11-17 14:01:42 +0200

    TEST 0000

---
+++ js/testtest.js
@@ -0,0 +1,25 @@
+// function digitize(n) {
+// //     return Array.from(String(n), Number).reverse();
+// //   }
+
+//   function digitize(n) {
+//     return String(n).split('').map(Number).reverse()
+//   }
+
+//  const arreyRevers= digitize(123456);
+
+//  console.log(arreyRevers)
+
+function booleanToString(b){
+    //your code here
+    if (b) {
+        return console.log(true)
+    } 
+        return console.log(false)
+    
+  }
+
+
+booleanToString(true), "true", 'When we pass in true, we want the string "true" as output'
+booleanToString(false), "false", 'When we pass in false, we want the string "false" as output'
+
